bf387c08f95fb2074f83a5fcea4824c828398d96
derrick matheny <mathenyderrick@gmail.com>
2021-04-21 23:45:52 -0400

    working on navbar -- need to figure out how to use FontAwesomeICons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div className="App">
- <h2>Mediocre At Best</h2>
-    </div>
+   <Navbar/>
   );
 }
 
