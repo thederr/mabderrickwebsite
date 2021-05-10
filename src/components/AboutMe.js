7cfce3d2192aba8439ec8d67f54d271443ac92c9
derrick matheny <mathenyderrick@gmail.com>
2021-05-10 06:02:45 -0400

    started the about me section

---
+++ src/components/AboutMe.js
@@ -0,0 +1,25 @@
+import React from "react";
+import author from "./summit-of-spruce-knob.jpg";
+const AboutMe = () => {
+  return (
+    <div className='container py-5'>
+      <div className='row'>
+        <div className='col-lg col-xm-12'>
+          <div className='photo-wrap'>
+            <img className="profile-img" src={author} alt='author' />
+          </div>
+        </div>
+        <div className='col-lg col-xm-12'></div>
+        <h1 className="about-heading">about me</h1>
+        <p>
+          Hello my name is Derrick. I'm a mining engineer who's gradually
+          getting more and more into web design and web development. Follow
+          along as I try to make sense of new-to-me technologies such as
+          Reactjs, Firebase, AWS and more.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
