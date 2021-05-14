9ecf06ea38153947dc3f8f1de860e464a4e9e884
derrick matheny <mathenyderrick@gmail.com>
2021-05-14 06:04:22 -0400

    finished the services markup and such, changed the typed text accidentally will need to fix that bad boy

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,69 +1,73 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee, faDesktop, faDraftingCompass, faFileCode } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCoffee,
+  faDesktop,
+  faDraftingCompass,
+  faFileCode,
+} from "@fortawesome/free-solid-svg-icons";
 import { faAws } from "@fortawesome/free-brands-svg-icons";
 
 function Services() {
   return (
     <div className='services'>
-      <div className='py-5'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-md-3 col-sm-6'>
-              <div className='box'>
-                <div className='circle'>
-                  <FontAwesomeIcon icon={faFileCode} size='2x' />
-                </div>
+      <h1 className='py-5'>My Services</h1>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-3 col-sm-6'>
+            <div className='box'>
+              <div className='circle'>
+                <FontAwesomeIcon className="icon" icon={faFileCode} size='2x' />
+              </div>
 
-                <h3>Web Development</h3>
-                <p>
-                  Utilizing the latest technologies: a Reactjs with Firebase and
-                  Amazon Web Services, we build Full Stack Applications that
-                  scale from 1 to 10 million users
-                </p>
-              </div>
+              <h3>Web Development</h3>
+              <p>
+                Utilizing the latest technologies: a Reactjs with Firebase and
+                Amazon Web Services, we build Full Stack Applications that scale
+                from 1 to 10 million users
+              </p>
             </div>
-            {/*-*/}
-            <div className='col-md-3 col-sm-6'>
-              <div className='box'>
+          </div>
+          {/*-*/}
+          <div className='col-md-3 col-sm-6'>
+            <div className='box'>
               <div className='circle'>
-                  <FontAwesomeIcon icon={faDesktop} size='2x' />
-                </div>
-                <h3>Web Design</h3>
-                <p>
-                  Working with Bootstrap, Material-UI, FontAwesome and the like,
-                  we make sure your website not only keeps up with the times,
-                  but it's ready for every sized screen for every sized mobile,
-                  laptop, and desktop size.
-                </p>
+                <FontAwesomeIcon className="icon" icon={faDesktop} size='2x' />
               </div>
+              <h3>Web Design</h3>
+              <p>
+                Working with Bootstrap, Material-UI, FontAwesome and the like,
+                we make sure your website not only keeps up with the times, but
+                it's ready for every sized screen for every sized mobile,
+                laptop, and desktop size.
+              </p>
             </div>
-            {/*-*/}
-            <div className='col-md-3 col-sm-6'>
-              <div className='box'>
+          </div>
+          {/*-*/}
+          <div className='col-md-3 col-sm-6'>
+            <div className='box'>
               <div className='circle'>
-                  <FontAwesomeIcon icon={faAws} size='2x' />
-                </div>
-                <h3>AWS Migration</h3>
-                <p>
-                  Working with small to medium sized companies offloading costly
-                  infastrcture and replacing inhouse servers with serverless
-                  archtecture from Amazon.
-                </p>
+                <FontAwesomeIcon className="icon" icon={faAws} size='2x' />
               </div>
+              <h3>AWS Migration</h3>
+              <p>
+                Working with small to medium sized companies offloading costly
+                infastrcture and replacing inhouse servers with serverless
+                archtecture from Amazon.
+              </p>
             </div>
-            {/*-*/}
-            <div className='col-md-3 col-sm-6'>
-              <div className='box'>
+          </div>
+          {/*-*/}
+          <div className='col-md-3 col-sm-6'>
+            <div className='box'>
               <div className='circle'>
-                  <FontAwesomeIcon icon={faDraftingCompass} size='2x' />
-                </div>
-                <h3>AutoCAD Modeling</h3>
-                <p>
-                  Freelance AutoCAD servces are avalible upon request. Here are
-                  a few projects I've worked on over the years.
-                </p>
+                <FontAwesomeIcon className="icon" icon={faDraftingCompass} size='2x' />
               </div>
+              <h3>AutoCAD Modeling</h3>
+              <p>
+                Freelance AutoCAD servces are avalible upon request. Here are a
+                few projects I've worked on over the years.
+              </p>
             </div>
           </div>
         </div>
@@ -77,7 +81,7 @@ export default Services;
 {
   /* Heres Some Example FontAwesome Syntax
 
-
+ className="icon"
 // Light:
 <FontAwesomeIcon icon={["fal", "coffee"]} />
 // Regular:
