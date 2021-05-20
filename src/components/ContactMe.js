5996afbd0ce962ee0d49d6bea11cc4646628d2ff
derrick matheny <mathenyderrick@gmail.com>
2021-05-19 21:46:36 -0400

    hilarious that I've been trying to debug a non issue, ofc the place holder text was gray. the orange showed up when I started typing in the input boxes

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -6,8 +6,8 @@ const ContactMe = () => {
       <div className='text-center'>
         <h1> contact me </h1>
         <p>
-          Please fill out the form and describe your project needs and I'll
-          contact you as quick as possible, unlike Lakota Software Solutions. 
+          Please fill out the form, describe your project needs and I'll
+          contact you as quick as possible, unlike Lakota Software Solutions.
         </p>
       </div>
       <div className='container'>
@@ -15,50 +15,50 @@ const ContactMe = () => {
           <div className='col-md-6 col-xs-12'>
             {/* NAME INPUT */}
             <input
-            id="name"
-            type="text"
+              id='name'
+              type='text'
               className='form-control'
               placeholder='Name'
               name='name'
             ></input>
             {/* PHONE INPUT */}
             <input
-            id="phone"
-            type="text"
+              id='phone'
+              type='text'
               className='form-control'
               placeholder='Phone Number'
               phone='phone'
-            ></input>
+            />
             {/* EMAIL INPUT */}
             <input
-            id="email"
-            type="email"
+              id='email'
+              type='email'
               className='form-control'
               placeholder='Email Address'
               email='email'
-            ></input>
+            />
             {/* SUBJECT INPUT */}
             <input
-            id="subject"
-            type="text"
+              id='subject'
+              type='text'
               className='form-control'
               placeholder='Subject'
               subject='subject'
-            ></input>
+            />
           </div>
 
-            <div className='col-md-6 col-xs-12'>
-              {/* TEXT INPUT */}
-              <textarea
-              id="description"
-              type="text"
-              className="form-control"
-              placeholder="Please give a breif description of your project needs"
-              >
-              </textarea>
-              <button className="btn-main-offer contact-btn" type="submit">contact me</button>
-              
-            </div>
+          <div className='col-md-6 col-xs-12'>
+            {/* TEXT INPUT */}
+            <textarea
+              id='description'
+              type='text'
+              className='form-control'
+              placeholder='Please give a breif description of your project needs'
+            ></textarea>
+            <button className='btn-main-offer contact-btn' type='submit'>
+              contact me
+            </button>
+          </div>
         </div>
       </div>
     </div>
