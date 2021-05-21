291676eae4e3aafc752db2fe6a9234da3c77c7b1
derrick matheny <mathenyderrick@gmail.com>
2021-05-20 21:25:43 -0400

    contact form is set up with emailjs and works, working on resetting the form and not sending on webpage reset tomorrow morning

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,65 +1,120 @@
-import React from "react";
+import React,{useState} from "react";
+import emailjs from "emailjs-com"
+import { useForm } from "react-hook-form";
+
 
 const ContactMe = () => {
+    const {register, handleSubmit,errors}   =   useForm();  
+   
+    const serviceID="service_ID";
+    const templateID="template_ID";
+    const userID=   "user_vxkbeuVrWtAPE7JWl0sms"
+    
+    const onSubmit=(data,resetting) =>{
+        sendEmail(
+            serviceID,
+            templateID,
+            {
+                name:data.name,
+                phone:data.phone,
+                email:data.email,
+                subject:data.subject,
+                description:data.description
+            },
+            userID
+        )
+        resetting.target.reset()
+    }
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('serice_ID', 'template_ID', variables, 'user_vxkbeuVrWtAPE7JWl0sms')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      }
+    
   return (
     <div className='contacts'>
       <div className='text-center'>
         <h1> contact me </h1>
         <p>
-          Please fill out the form, describe your project needs and I'll
-          contact you as quick as possible, unlike Lakota Software Solutions.
+          Please fill out the form and describe your project needs. I'll contact
+          you as soon as possible.
         </p>
       </div>
       <div className='container'>
+          <form onSubmit={sendEmail}>
         <div className='row'>
           <div className='col-md-6 col-xs-12'>
             {/* NAME INPUT */}
-            <input
-              id='name'
-              type='text'
-              className='form-control'
-              placeholder='Name'
-              name='name'
-            ></input>
+
+            <div className='text-center'>
+              <input
+                id='name'
+                type='text'
+                className='form-control'
+                placeholder='Name'
+                name='name'
+              />
+              <div className='line'></div>
+            </div>
             {/* PHONE INPUT */}
-            <input
-              id='phone'
-              type='text'
-              className='form-control'
-              placeholder='Phone Number'
-              phone='phone'
-            />
+            <div className='text-center'>
+              <input
+                id='phone'
+                type='text'
+                className='form-control'
+                placeholder='Phone Number'
+                name='phone'
+              />
+              <div className='line'></div>
+            </div>
+
             {/* EMAIL INPUT */}
-            <input
-              id='email'
-              type='email'
-              className='form-control'
-              placeholder='Email Address'
-              email='email'
-            />
+            <div className='text-center'>
+              <input
+                id='email'
+                type='email'
+                className='form-control'
+                placeholder='Email Address'
+                name='email'
+              />
+              <div className='line'></div>
+            </div>
             {/* SUBJECT INPUT */}
-            <input
-              id='subject'
-              type='text'
-              className='form-control'
-              placeholder='Subject'
-              subject='subject'
-            />
+            <div className='text-center'>
+              <input
+                id='subject'
+                type='text'
+                className='form-control'
+                placeholder='Subject'
+                name='subject'
+              />
+              <div className='line'></div>
+            </div>
           </div>
 
           <div className='col-md-6 col-xs-12'>
             {/* TEXT INPUT */}
-            <textarea
-              id='description'
-              type='text'
-              className='form-control'
-              placeholder='Please give a breif description of your project needs'
-            ></textarea>
+            <div className='text-center'>
+              <textarea
+                id='description'
+                type='text'
+                className='form-control'
+                placeholder='Please give a breif description of your project needs'
+              ></textarea>
+              <div className='line'></div>
+            </div>
             <button className='btn-main-offer contact-btn' type='submit'>
               contact me
             </button>
           </div>
         </div>
+        </form>
       </div>
     </div>
   );
