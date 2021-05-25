2b9911571539abb04538fe34168df061454c586a
derrick matheny <mathenyderrick@gmail.com>
2021-05-24 20:28:41 -0400

    footer finished added linkedin, facebook, and twitter react icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,13 @@
+import { NUMBER_BINARY_OPERATORS } from "@babel/types";
 import React from "react";
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
+} from "react-share";
 
 const Footer = () => {
   return (
@@ -20,23 +29,62 @@ const Footer = () => {
         <div className=' col-lg-3 col-md-2 col-sm-6'>
           <div className='row'>
             <div className='col'>
-            <a className="footer-nav">Home</a>
-                <br />
-                <a className="footer-nav">About me</a>
-                <br />
-                <a className="footer-nav">Services</a>
-              </div>
-              <div className="col">
-                <a className="footer-nav">Experience</a>
-                <br />
-                <a className="footer-nav">Portfolio</a>
-                <br />
-                <a className="footer-nav">Contacts</a>
+              <a className='footer-nav'>Home</a>
+              <br />
+              <a className='footer-nav'>About me</a>
+              <br />
+              <a className='footer-nav'>Services</a>
+            </div>
+            <div className='col'>
+              <a className='footer-nav'>Experience</a>
+              <br />
+              <a className='footer-nav'>Portfolio</a>
+              <br />
+              <a className='footer-nav'>Contacts</a>
+            </div>
+          </div>
+        </div>
+        <div className='col-lg-5 col-md-5 col-sm-6 align-items-center'>
+          <div className='d-flex justify-content-center'>
+            <LinkedinShareButton
+                
+              url={"linkedin.com/in/derrick-matheny-930694181"}
+              quote={"Mediocre at Best"}
+              hashtag='#MABtechnologies'
+            >
+              <LinkedinIcon />
+            </LinkedinShareButton>
+
+
+            <FacebookShareButton
+                
+              url={"https://www.facebook.com/WVtourism/"}
+              quote={"Mediocre at Best"}
+              hashtag='#MABtechnologies'
+            >
+                <FacebookIcon/>
+            </FacebookShareButton>
+
+            <TwitterShareButton
+                
+              url={"https://twitter.com/WVTourism"}
+              quote={"Mediocre at Best"}
+              hashtag='#MABtechnologies'
+            >
+                <TwitterIcon/>
+            </TwitterShareButton>
+
+
+
+          </div>
+          <p className="pt-3 text-center">
+              Copyright&copy;
+              {new Date().getFullYear()} MAB Technologies | All Rights Reserved
+          </p>
           </div>
         </div>
       </div>
-    </div>
-    </div>
+    
   );
 };
 
